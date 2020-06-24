53fb8288fed04505251e13ee2889c8d0cc0bbcbf
Alex Lelievre <alex@folabs.com>
2020-06-24 10:15:40 -0700

    Much cleaner implementation-  mainly that it actually works. The old code was pretty messed up and only got lucky to work which always amazes me (which is why I really need to work with a debugger).

--- vx8nmea.ino
+++ vx8nmea.ino
@@ -67,7 +67,7 @@ enum
 #endif
 
 #define kGPGGA_SkipParams kHDOP
-#define kGPRMC_SkipParams kRMCMessageID
+#define kGPRMC_SkipParams kRMCEWIndicator
 
 #define kSerialInputPin 3
 
@@ -84,18 +84,14 @@ static char s_date[16]     = {};
 
 static int  s_writeIndex = 0;
 static int  s_comma      = 0;
-
 static bool s_latch      = false;
-static bool s_utc_latch  = false;
-static bool s_date_latch = false;
+static bool s_mute       = false;
 
 static bool s_processingGGA = false;
 static bool s_processingRMC = false;
-static bool s_processingZDA = false;
 
 static const char* kGGAPrefix = "$GPGGA";
 static const char* kRMCPrefix = "$GPRMC";
-static const char* kZDAPrefix = "$GPZDA";
 
 
 
@@ -111,18 +107,25 @@ bool checkForGPRMC( const char* check )
 }
 
 
-void padZeros( const char* input, int full_size )
+
+void outputPaddedString( const char* string, int padded_size )
 {
-    if( !input )
+    if( !string )
         return;
+
+    int input_size = strlen( string );
+    int pad_amount = padded_size - input_size;
+
+//    kOutput.print( "\n\ninput: " ); kOutput.println( string );
+//    kOutput.print( "pad_amount : " ); kOutput.println( pad_amount );
     
-    int input_size = strlen( input );
-    int pad_amount = full_size - input_size;
-    if( pad_amount <= 0 )
-        return;
-    
-    for( int i = 0; i < pad_amount; i++ )
-        kOutput.write( '0' );
+    if( pad_amount > 0 )
+    {
+        for( int i = 0; i < pad_amount; i++ )
+            kOutput.write( '0' );
+    }
+    for( int i = 0; i < input_size; i++ )
+        kOutput.write( string[i] );
 }
 
 
@@ -178,142 +181,94 @@ void loop()
     while( kInput.available() )
     {
         int input = kInput.read();
-        // If anything comes in kInput (pins 0 & 1)
-        s_buffer[s_writeIndex] = input;
         
         // process our buffer
-        if( input == ',' || input == '*' )
+        if( input == ',' )
         {
-            if( input == ',' )
-            {
-                ++s_comma;
-                s_writeIndex = 0;
-                s_latch      = false;
-
-//                kOutput.print( "s_buffer: " );
-//                kOutput.println( s_buffer );
-//                kOutput.println( "\n" );
-            }
+            s_buffer[s_writeIndex] = '\0';
+            ++s_comma;
+            s_writeIndex = 0;
+            s_latch      = false;
             
             if( !s_processingGGA && checkForGPGGA( s_buffer ) )
             {
                 s_processingGGA = true;
-                s_buffer[s_writeIndex] = input; // put that character at the front for further processing
             }
             else if( !s_processingRMC && checkForGPRMC( s_buffer ) )
             {
                 s_processingRMC = true;
-                s_buffer[s_writeIndex] = input; // put that character at the front for further processing
             }
         }
+        else
+          s_buffer[s_writeIndex++] = input;
+
         
         if( s_processingGGA )
         {
-            // see if we should ignore the input or not...
-            if( (input != '\n') &&  (input != ',') && (input != '*') && s_comma > kGPGGA_SkipParams )
-            {
-                if( s_comma == kAltiude )
-                {
-                    if( !s_latch )                      // needs to be rewritten from xx.y to 000xx.y (MSL Altitude)
-                    {
-                        s_buffer[++s_writeIndex] = '\0';
-                        padZeros( s_buffer, 4 );
-                        s_latch = true;
-                    }
-                    kOutput.write( input );
-                }
-                else if( s_comma >= kAltUnit )
-                    kOutput.write( input );
-            }
-            else
-            {
-                if( input == '*' ) // checksum
-                    s_processingGGA = false;
-                kOutput.write( input );
-            }
-        }
-        else if( s_processingRMC )
-        {
-            // see if we should ignore the input or not...
-            if( (input != '\n') /*&& (input != ',')*/ && (input != '*') && s_comma > kGPRMC_SkipParams )
-            {
-                if( s_comma == kRMCUTCTime )
-                {
-                    kOutput.write( input );
-                }
-                else if( s_comma == kRMCStatus ) // we have the full UTC time in there as status comes right after it...
-                {
-                    if( !s_utc_latch )  // we need to hold on to this
-                    {
-                        s_buffer[s_writeIndex] = '\0';  // stomp after the comma at the end
-                        strcpy( s_utc, &s_buffer[1] );  // ignore comma at beginning
-                        s_utc_latch = true;
-                    }  
-                    kOutput.write( input );
-                }
-                else if( s_comma == kRMCSpeed )
-                {
-                    if( !s_latch )                      // needs to be rewritten from x.yy to 000x.yy (Speed over ground)
-                    {
-                        s_buffer[++s_writeIndex] = '\0';
-                        padZeros( s_buffer, 5 );
-                        s_latch = true;
-                    }
-                    kOutput.write( input );
-                }
-                else if( s_comma == kRMCDate )
-                {
-                    if( !s_date_latch )  // we need to hold on to this
-                    {
-                        s_buffer[s_writeIndex] = '\0';
-                        strcpy( s_date, &s_buffer[1] );
-                        s_date_latch = true;
-                    } 
-                    kOutput.write( input );
-                }
-                else if( s_comma >= kRMCStatus )
-                    kOutput.write( input );
-            }
-            else
-            {
-//                if( input == '*' ) // checksum
-//                    s_processingRMC = false;
-                kOutput.write( input );
-            }
-            
-        }
-        else if( s_processingZDA )
-        {
-            kOutput.print( kZDAPrefix );
-            kOutput.print( "," );
+              if( s_comma == kAltiude )
+              {
+                  // do not write output until we have full field
+                  s_mute = true;
+              }
+              else if( s_comma == kAltUnit )
+              {
+                  s_mute = false;
 
-            // output the time and date
-            kOutput.print( s_utc ); // has comma trailing
-            kOutput.println( "23,06,2020,,*55" );
-            s_processingZDA = false;
-            s_writeIndex = 0;
-            s_comma = 0;
-            kOutput.write( input ); // put that character at the front for further processing
+                  if( !s_latch )   
+                  {
+                      // we now have the full altitude field buffered up, so pad it while outputting
+                      // needs to be rewritten from xx.y to 000xx.y (MSL Altitude)
+                      kOutput.write( ',' );
+                      outputPaddedString( s_buffer, 7 );
+                      s_latch = true; // do this only once at the beginning
+                  }
+              }
         }
-        else
+
+
+        if( s_processingRMC )
         {
-            kOutput.write( input );
+              if( s_comma == kRMCSpeed )
+              {
+                  // do not write output until we have full field
+                  s_mute = true;
+              }
+              else if( s_comma == kRMCCourse )
+              {
+                  // output altitude we just buffered up while buffering up the course info
+                  if( !s_latch )   
+                  {
+                      // needs to be rewritten from x.yy to 000x.yy (Speed over ground)
+                      kOutput.write( ',' );
+                      outputPaddedString( s_buffer, 7 );
+                      s_latch = true; // do this only once at the beginning
+                  }
+              }
+              else if( s_comma == kRMCDate )
+              {
+                  s_mute = false;
+                  
+                  // spit out the course info we just finished buffering.
+                  if( !s_latch )   
+                  {
+                      // needs to be rewritten from xx.yy to 0xx.yy
+                      kOutput.write( ',' );
+                      outputPaddedString( s_buffer, 6 );
+                      s_latch = true; // do this only once at the beginning
+                  }
+             }
         }
+
+        if( !s_mute )
+            kOutput.write( input );
         
         if( input == '\n' )
         {
-            // we just finished processing RMC message so send out a ZDA message
-//            if( s_processingRMC )
-//              s_processingZDA = true;     // now do the ZDA string after consuming the checksum
-            
             s_writeIndex = 0;
             s_comma = 0;
             s_processingRMC = false;
-            s_utc_latch  = false;
-            s_date_latch = false;
+            s_processingGGA = false;
         }
-        else
-            ++s_writeIndex;
     }
     
     enter_Sleep();
